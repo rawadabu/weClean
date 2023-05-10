934d3e015666c7ceaeaa01b4a0946b19828042c2
rawadabu <rawadabusaleh0@gmail.com>
2023-05-10 12:34:49 +0300

    Organizing

--- Elbandora/src/app/app.module.ts
+++ Elbandora/src/app/app.module.ts
@@ -8,9 +8,20 @@ import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, DescriptionComponent, AboutComponent, ServicesComponent, FeedbackComponent, SignupComponent],
+  declarations: [
+    AppComponent,
+    DescriptionComponent,
+    AboutComponent,
+    ServicesComponent,
+    FeedbackComponent,
+    SignupComponent,
+    NavbarComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
