64fe2e7ccb06ea9ba420a5fa3cc9d5b77e1066da
rawadabu <rawadabusaleh0@gmail.com>
2023-05-11 13:08:02 +0300

    Organizing components

--- Elbandora/src/app/components/services/services.component.ts
+++ Elbandora/src/app/components/services/services.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css', '../header/header.component.css'],
 })
-export class ServicesComponent {
-
-}
+export class ServicesComponent {}
