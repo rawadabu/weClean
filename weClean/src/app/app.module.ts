98ae715b993e2211e36f67f63d6a705a0479eb35
rawadabu <rawadabusaleh0@gmail.com>
2023-05-12 14:38:58 +0300

    Footer added&CSS manipulation

--- weClean/src/app/app.module.ts
+++ weClean/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FeedbackComponent } from './components/feedback/feedback.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { HeaderComponent } from './components/header/header.component';
     SignupComponent,
     NavbarComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
