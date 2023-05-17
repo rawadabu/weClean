a13ffc977eafb22145644d2d83bc633f135cb7b0
rawadabu <rawadabusaleh0@gmail.com>
2023-05-17 17:21:19 +0300

    Implementing feedback

--- weClean/src/app/components/feedback/feedback.component.ts
+++ weClean/src/app/components/feedback/feedback.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { User } from '../../models/user.model';
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  styleUrls: ['./feedback.component.css'],
 })
 export class FeedbackComponent {
-
+  user: User = {
+    id: '123123',
+    firstName: 'Rawad',
+    lastName: 'Abusaleh',
+    email: 'rawad@gmail.com',
+    description: 'Best financial decision ever!!',
+  };
 }
