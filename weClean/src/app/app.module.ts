9ccea02956d41dc83c744825e1c1cb834fbffb14
rawadabu <rawadabusaleh0@gmail.com>
2023-05-18 13:19:55 +0300

    app module routes updated

--- weClean/src/app/app.module.ts
+++ weClean/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { DescriptionComponent } from './components/description/description.compo
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
-import { contactusComponent } from './components/contact/contact-us.component';
+import { contactusComponent } from './components/contact-us/contact-us.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
