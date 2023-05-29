e7c3da45f0538500374d0ecc52bcc4c598943d9c
rawadabu <rawadabusaleh0@gmail.com>
2023-05-29 09:02:18 +0300

    Implemented scrollToTop

--- weClean/src/app/app.component.ts
+++ weClean/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ViewportScroller } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { ViewportScroller } from '@angular/common';
 })
 export class AppComponent {
   title = 'weClean';
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  scrollToTop(): void {
+    // scroll to the top of the body
+    return this.document.body.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'start',
+    });
+  }
 }
