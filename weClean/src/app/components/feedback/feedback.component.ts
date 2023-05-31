3770e4a734a355aab95603888188ac6e372d6d73
rawadabu <rawadabusaleh0@gmail.com>
2023-05-31 18:18:39 +0300

    Models css combined with style.css, navbar minimized, and feedback db collected

--- weClean/src/app/components/feedback/feedback.component.ts
+++ weClean/src/app/components/feedback/feedback.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
 import { FeedbackService } from 'src/app/services/feedback.service';
-import { Observable } from 'rxjs';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from 'src/enviroments/enviroment';
+import { Feedback } from '../../models/feedback.model';
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css'],
 })
 export class FeedbackComponent implements OnInit {
-  ngOnInit(): void {
-    console.log(this.feedbackService.getFeedbacks());
-    this.feedbackService.getFeedbacks();
-  }
+  feedbacks: Feedback[] = [];
   user: User = {
     firstName: 'Rawad',
     lastName: 'Abusaleh',
@@ -22,4 +21,12 @@ export class FeedbackComponent implements OnInit {
       'https://images.squarespace-cdn.com/content/v1/5e6ece70bd2f8a6de8472818/714f685e-d0ba-40f9-8bb2-38722c1fd29c/Tiny+Avatar.png?format=1000w',
   };
   constructor(private feedbackService: FeedbackService) {}
+
+  ngOnInit(): void {
+    this.loadFeedbacks();
+  }
+
+  async loadFeedbacks(): Promise<void> {
+    this.feedbacks = await this.feedbackService.getFeedbacks();
+  }
 }
