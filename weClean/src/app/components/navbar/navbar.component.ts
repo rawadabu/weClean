a9e19b05032d9126eeef939573d9bcdcc2c18965
rawadabu <rawadabusaleh0@gmail.com>
2023-06-07 09:52:06 +0300

    Implemented Log out and feedback for signed in user

--- weClean/src/app/components/navbar/navbar.component.ts
+++ weClean/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +9,45 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class NavbarComponent implements OnInit {
   @Input() featureSelected = new EventEmitter<string>();
   @Output() openModal: EventEmitter<void> = new EventEmitter();
+  public isLoggedIn = false;
+
+  constructor() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      console.log('Auth state changed. User:', user);
+      this.isLoggedIn = !!user;
+    });
+  }
 
   ngOnInit(): void {
-    // Add any initialization logic here
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        this.isLoggedIn = true;
+      } else {
+        // User is signed out
+        this.isLoggedIn = false;
+      }
+    });
   }
 
   public isModalVisible = false;
   toggleModal(): void {
     this.openModal.emit();
+  }
+
+  logOff() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log('User signed out.');
+        this.isLoggedIn = false;
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log('Error signing out:', error);
+      });
   }
 }
